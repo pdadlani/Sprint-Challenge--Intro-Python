0358b3c1718ccdbfcdcffd2921bfd1468226c6cf
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-04-10 08:12:48 -0700

    starting from scratch.

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,12 +4,10 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels=4):
+    def __init__(self, num_wheels):
         self.num_wheels = num_wheels
 
     # TODO
-    def drive(self):
-        return "vroooom"
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -19,13 +17,6 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
-
-class Motorcycle(GroundVehicle):
-    def __init__(self, num_wheels=2):
-        super().__init__(num_wheels=num_wheels)
-
-    def drive(self):
-        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -39,5 +30,3 @@ vehicles = [
 
 # TODO
 
-for vehicle in vehicles:
-    print(vehicle.drive())
